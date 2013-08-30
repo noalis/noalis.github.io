4ea18f5fea56664314c83a55ce44dd279aac1699
Lior Brauer <lior@goroomer.com>
2013-08-30 17:06:39 +0300

    added cat and lior

--- src/components.js
+++ src/components.js
@@ -6,9 +6,11 @@ Crafty.c('Rock', {
 
 Crafty.c('GuyPlayer', {
   init: function(){
-    this.requires('2D, Canvas, Image, Gravity, Twoway, Collision')
-    .image('assets/mario1.png')
-    .attr({ w: Game.map_grid.tile.width*2, h: Game.map_grid.tile.height*2})
+    this.requires('2D, DOM, Image, Gravity, Twoway, Collision')
+    .attr({width: 10, height: 10})
+    .image('assets/lior_stand.png')
+    //  .attr({ w: 1, h: 1})
+    // .attr({ w: Game.map_grid.tile.width*2, h: Game.map_grid.tile.height*2})
     // .color("green")
     .gravity('Rock')
     .gravityConst(0.8)
@@ -16,5 +18,22 @@ Crafty.c('GuyPlayer', {
     // .onHit('Rock', function(e){
     //   console.log(e);
     // })
+  }
+});
+
+Crafty.c('Cat', {
+  speedX: -4,
+  minX: 400,
+  maxX: 500,
+  init: function(){
+    this.requires('2D, Canvas, Image, Collision')
+    .image('assets/cat.png');
+  },
+  go: function(){
+    this.x += this.speedX;
+    if (this.speedX < 0 && this.x <= this.minX || this.speedX > 0 && this.x >= this.maxX) { this.speedX *= -1; }
+
+    if (this.speedX < 0) { this.flip("X"); }
+    else { this.unflip("X"); }
   }
 });
--- src/game.js
+++ src/game.js
@@ -7,6 +7,9 @@ Game = {
       height: 32
     }
   },
+  lives: 3,
+  hasRing: false,
+  
   width: function() {
     return this.map_grid.width * this.map_grid.tile.width;
   },
@@ -14,16 +17,22 @@ Game = {
   height: function() {
     return this.map_grid.height * this.map_grid.tile.height;
   },
-
+  reset: function(){
+    
+  },
   start: function(){
     Crafty.init(Game.width(), Game.height());
     Crafty.background('#78bbff');
+    
+    // build map with rocks
     var i, j;
     for (i=0; i<Game.map_grid.width; i+=5) {
       Crafty.e("Rock").attr({x: Game.map_grid.tile.width*i, y: (Game.map_grid.height-1)*Game.map_grid.tile.height});
     }
-      Crafty.e("Rock").attr({x: 64, y: (Game.map_grid.height-2)*Game.map_grid.tile.height});
-
+    
+    Crafty.e("Rock").attr({x: 64, y: (Game.map_grid.height-2)*Game.map_grid.tile.height});
+    var cat = Crafty.e('Cat').attr({x: 500, y:(Game.map_grid.height-2)*Game.map_grid.tile.height});
+    
     // for (i=0; i<Game.map_grid.width; i++) {
     //   for (j=0; j<Game.map_grid.height; j++) {
     //     if (i === 0 || i === Game.map_grid.width-1) {
@@ -31,7 +40,10 @@ Game = {
     //     }
     //   }
     // }
+    
+
     var player = Crafty.e('GuyPlayer').attr({x: 0, y:0});
+    window.player = player;
     player.bind("EnterFrame", function(){
         // left bound
         if (this.x<=0) { this.x = 0; }
@@ -45,13 +57,25 @@ Game = {
           // console.log("to to reset");
         }
         // check side collision with rocks and do not allow overlap
-        var collision_data = this.hit("Rock");
-        if (collision_data) {
-          var rock_x = collision_data[0].obj.x;
-          var rock_w = collision_data[0].obj.w;
+        var hit_rock = this.hit("Rock");
+        if (hit_rock) {
+          var rock_x = hit_rock[0].obj.x;
+          var rock_w = hit_rock[0].obj.w;
           if (rock_x > this.x) { this.x = rock_x - this.w; }
           else { this.x = rock_x + rock_w; }
         }
+
+        var hit_cat = this.hit("Cat");
+        if (hit_cat) {
+          // console.log('cat hit!');
+          Game.lives-=1;
+          this.x=0;
+          this.y=0;
+        }
     });
+    cat.bind("EnterFrame", function(){
+      this.go();
+    });
+    Crafty.e("2D, Canvas, Text").attr({ x: 100, y: 100 }).text("Lives: " + Game.lives);
   }
 };

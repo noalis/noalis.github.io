9c5f0239169f3952434cdc8d9f7b38fd230e7fcb
Lior Brauer <lior@goroomer.com>
2013-09-07 20:10:38 +0300

    need an opening screen

--- src/game.js
+++ src/game.js
@@ -21,13 +21,11 @@ Game = {
     Crafty.init(Game.width(),Game.height(), "stage");
     Crafty.scene("loading", function() {
       Crafty.background("#000");
-      Crafty.e("2D, DOM, Text")
-        .attr({ w: 100, h: 20, x: 150, y: 120 })
-        .text("Loading")
-        .css({ "text-align": "center"})
+      Crafty.e("2D, Canvas, Text")
+        .attr({ x: 320, y: Game.height()/2+20})
+        .text("Loading...")
+        .textFont({ size: '108px' })
         .textColor("#FFFFFF");
-      Crafty.image_whitelist.push("wav");
-      Crafty.image_whitelist.push("png");
       Crafty.load(
         [
           "assets/sounds/jump.wav",
